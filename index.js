dd9f7a21605b3b9b7d42765ffcf2b9d638c5dc7e
dmtask <dmtask@gmx.de>
2021-06-12 14:39:59 +0200

    Mehr Befehle eingebaut

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const {get_time} = require('./src/get_time');
 const {stop_timer} = require('./src/stop');
 
@@ -7,10 +9,25 @@ const workspace_id = process.env.WORKSPACE_ID;
 const user_id = process.env.USER_ID;
 const api_key = process.env.API_KEY;
 
+const commandList = '- node index.js getTime [DATUMSANGABE] (DD.MM.YYYY)\n- node index.js getTime yesterday\n' +
+    '- node index.js getTime today\n- node index.js stopTimer\n\n';
+
 if (process.argv[2] === 'getTime') {
-    get_time(workspace_id, user_id, api_key, process.argv[3]);
+    if (process.argv[3] === 'yesterday') {
+        let yesterday = moment().subtract(1, 'days').format('DD.MM.YYYY');
+
+        get_time(workspace_id, user_id, api_key, yesterday);
+    } else if (process.argv[3] === 'today') {
+        let today = moment().format('DD.MM.YYYY');
+
+        get_time(workspace_id, user_id, api_key, today);
+    } else {
+        get_time(workspace_id, user_id, api_key, process.argv[3]);
+    }
 } else if (process.argv[2] === 'stopTimer') {
     stop_timer(workspace_id, user_id, api_key);
+} else if (process.argv[2] === 'help') {
+    console.log('DM - Task clockify Scripts. Folgende Befehle gibt es aktuell:\n\n' + commandList);
 } else {
-    console.error('Befehl nicht gefunden! Folgende Befehle gibt es aktuell:\n\n- node index.js getTime DATUMSANGABE\n- node index.js stopTimer\n\n');
+    console.error('Befehl nicht gefunden! Folgende Befehle gibt es aktuell:\n\n' + commandList);
 }
